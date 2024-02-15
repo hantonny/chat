a3cca30e7f24318c73281ebd3e7b83c052858536
Hantonny Korrea <hantonny.korrea@nastek.com.br>
2024-02-15 11:01:27 -0400

    Adicionar Imagem

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,7 +1,45 @@
+import 'dart:io';
+
+import 'package:chat/components/user_image_picker.dart';
+import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
-  const AuthForm({super.key});
+class AuthForm extends StatefulWidget {
+  final void Function(AuthFormData) onSubmit;
+
+  const AuthForm({super.key, required this.onSubmit});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _handleImagePick(File image) {
+    _formData.image = image;
+  }
+
+  void _showErrorMensage(String msg) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(msg),
+        backgroundColor: Theme.of(context).errorColor,
+      ),
+    );
+  }
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) return;
+
+    if (_formData.image == null && _formData.isSignup) {
+      return _showErrorMensage('Imagem não selecionada!');
+    }
+
+    widget.onSubmit(_formData);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,42 +48,84 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
+          key: _formKey,
           child: Column(children: [
+            if (_formData.isSignup)
+              UserImagePicker(
+                onImagePick: _handleImagePick,
+              ),
+            if (_formData.isSignup)
+              TextFormField(
+                key: const ValueKey('name'),
+                initialValue: _formData.name,
+                onChanged: (name) => _formData.name = name,
+                decoration: const InputDecoration(labelText: 'Nome'),
+                validator: (localName) {
+                  final name = localName ?? '';
+                  if (name.trim().length < 5) {
+                    return 'Nome deve ter no mínimo 5 caracteres';
+                  }
+                  return null;
+                },
+              ),
             TextFormField(
-              decoration: const InputDecoration(labelText: 'Nome'),
-            ),
-            TextFormField(
+              key: const ValueKey('email'),
+              initialValue: _formData.email,
+              onChanged: (email) => _formData.email = email,
               decoration: const InputDecoration(labelText: 'E-mail'),
+              validator: (localEmail) {
+                final email = localEmail ?? '';
+                if (!email.contains('@')) {
+                  return 'E-mail informado não é válido!';
+                }
+                return null;
+              },
             ),
             TextFormField(
+              key: const ValueKey('password'),
+              initialValue: _formData.password,
+              onChanged: (password) => _formData.password = password,
               obscureText: true,
               decoration: const InputDecoration(labelText: 'Senha'),
+              validator: (localPassword) {
+                final password = localPassword ?? '';
+                if (password.length < 6) {
+                  return 'Senha deve ter no mínimo 6 caracteres';
+                }
+                return null;
+              },
             ),
             const SizedBox(
               height: 12,
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: _submit,
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
                 shadowColor: MaterialStateProperty.all<Color>(Colors.blue),
               ),
-              child: const Text(
-                'Entrar',
-                style: TextStyle(color: Colors.white),
+              child: Text(
+                _formData.isLogin ? 'Entrar' : 'Cadastrar',
+                style: const TextStyle(color: Colors.white),
               ),
             ),
             const SizedBox(
               height: 12,
             ),
             TextButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  _formData.tooggleAuthMode();
+                });
+              },
               style: ButtonStyle(
                 shadowColor: MaterialStateProperty.all<Color>(Colors.blue),
               ),
-              child: const Text(
-                'Criar uma nova conta?',
-                style: TextStyle(color: Colors.blue),
+              child: Text(
+                _formData.isLogin
+                    ? 'Criar uma nova conta?'
+                    : 'Já possuí conta?',
+                style: const TextStyle(color: Colors.blue),
               ),
             )
           ]),
--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,17 +1,48 @@
 import 'package:chat/components/auth_form.dart';
+import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
 
   @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool _isLoading = false;
+  void _handleSubmit(AuthFormData formData) {
+    setState(() => _isLoading = true);
+    print('AuthPage...');
+    print(formData.email);
+    // setState(() => _isLoading = false);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.blue,
-      body: Center(
-          child: SingleChildScrollView(
-        child: AuthForm(),
-      )),
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(onSubmit: _handleSubmit),
+            ),
+          ),
+          if (_isLoading)
+            Container(
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+              ),
+              child: const Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.transparent,
+                  color: Colors.blue,
+                ),
+              ),
+            )
+        ],
+      ),
     );
   }
 }
