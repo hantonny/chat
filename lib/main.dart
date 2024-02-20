d67e9e186f88dfc04cf6fada5e4629fde02e140e
Hantonny Korrea <hantonny.korrea@nastek.com.br>
2024-02-20 16:50:03 -0400

    Atualização

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'dart:io';
+
 import 'package:chat/core/services/notification/chat_notification_service.dart';
 import 'package:chat/pages/auth_or_app_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: 'AIzaSyA59hYNxUemsgdnGXeqjrJ-XI3Pc0LnzmE',
+            appId: '1:527656722661:android:8d68af3688bfee2cc3b40c',
+            messagingSenderId: '527656722661',
+            projectId: 'chat0-edcd7',
+          ),
+        )
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
